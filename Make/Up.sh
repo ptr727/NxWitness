2d0c008e9871b4e658a5d3a1978c0fe51a2c2a97
Pieter Viljoen <ptr727@users.noreply.github.com>
2021-10-01 15:56:26 +0000

    Nx 4.3 R3 now again supports Ubuntu Focal

--- Make/Up.sh
+++ Make/Up.sh
@@ -3,9 +3,9 @@
 docker-compose --file Test.yml down --volumes
 docker-compose --file Test.yml up --detach
 
-echo "https://$HOSTNAME:7101/"
-echo "https://$HOSTNAME:7111/"
-echo "https://$HOSTNAME:7102/"
-echo "https://$HOSTNAME:7112/"
-echo "https://$HOSTNAME:7103/"
-echo "https://$HOSTNAME:7113/"
+echo "DW Spectrum:" "https://$HOSTNAME:7101/"
+echo "DW Spectrum-LSIO:" "https://$HOSTNAME:7111/"
+echo "Nx Witness:" "https://$HOSTNAME:7102/"
+echo "Nx Witness-LSIO:" "https://$HOSTNAME:7112/"
+echo "Nx Meta:" "https://$HOSTNAME:7103/"
+echo "Nx Meta-LSIO:" "https://$HOSTNAME:7113/"
