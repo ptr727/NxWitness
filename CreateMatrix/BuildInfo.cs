0e4638eefc0eff5e0bcaa5e7b008fd6d96e2a33c
Pieter Viljoen <ptr727@users.noreply.github.com>
2022-07-14 18:42:41 +0000

    Add CreateMatrix project

---
+++ CreateMatrix/BuildInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.AccessControl;
+
+namespace CreateMatrix;
+
+public class BuildInfo
+{
+    public static List<ImageInfo> CreateImages(List<ProductVersion> productList)
+    {
+        // Iterate through products
+        List<ImageInfo> imageList = new();
+        foreach (var productVersion in productList)
+        {
+            // Iterate through base names
+            foreach (var baseName in BuildInfo.BaseNames)
+            {
+                // Create images
+                imageList.AddRange(ImageInfo.CreateImages(productVersion, baseName, ""));
+            }
+        }
+
+        // Set branch as main
+        imageList.ForEach(item => item.Branch = "main");
+
+        // Create develop builds of NxMeta
+        List<ImageInfo> developList = new();
+        var nxMeta = productList.Find(item => item.Name.Equals("NxMeta", StringComparison.InvariantCultureIgnoreCase));
+        ArgumentNullException.ThrowIfNull(nxMeta);
+
+        // Iterate through base names
+        foreach (var baseName in BuildInfo.BaseNames)
+        {
+            // Create images
+            developList.AddRange(ImageInfo.CreateImages(nxMeta, baseName, "develop"));
+        }
+
+        // Set branch as develop
+        developList.ForEach(item => item.Branch = "develop");
+
+        // Add to list
+        imageList.AddRange(developList);
+
+        return imageList;
+    }
+
+    public static readonly string[] BaseNames = { "", "LSIO" };
+}
---
+++ CreateMatrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using CreateMatrix;
+using Serilog;
+using Serilog.Debugging;
+using Serilog.Sinks.SystemConsole.Themes;
+using System.IO;
+using System.Xml.Linq;
+
+// Configure serilog console logging
+SelfLog.Enable(Console.Error);
+LoggerConfiguration loggerConfiguration = new();
+loggerConfiguration.WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] {Message}{NewLine}{Exception}");
+Log.Logger = loggerConfiguration.CreateLogger();
+
+// Create default versions and schema
+// VersionJsonSchema.WriteDefaultsToFile(@"Version.json");
+// VersionJsonSchema.GenerateSchema(@"Version.schema.json");
+
+// Default file names
+string versionPath = @"Version.json";
+string matrixPath = @"Matrix.json";
+
+// Replace defaults from commandline if set
+if (args.Length == 2)
+{
+    versionPath = args[0];
+    matrixPath = args[1];
+}
+else 
+{
+    Log.Logger.Information("Commandline not specified, using defaults, Usage: CreateMatrix [VersionPath] [MatrixPath]");
+}
+Log.Logger.Information("VersionPath: {versionpath}, MatrixPath: {matrixpath}", versionPath, matrixPath);
+
+// Load versions
+var versionJson = VersionJsonSchema.FromFile(versionPath);
+Log.Logger.Information("Loaded {count} products from {path}", versionJson.Products.Count, versionPath);
+
+// Create images
+MatrixJsonSchema matrixJson = new();
+matrixJson.Images = BuildInfo.CreateImages(versionJson.Products);
+
+// Write matrix
+Log.Logger.Information("Writing {count} images to {path}", matrixJson.Images.Count, matrixPath);
+MatrixJsonSchema.ToFile(matrixPath, matrixJson);
